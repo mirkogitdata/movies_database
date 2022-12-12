24440e0a3a474b04509108bc152009efc8438f7c
Mirko Zivic <mirkoduartes@gmail.com>
2022-12-12 11:56:31 +0100

    Added ternary expression

--- src/components/MovieCard/index.js
+++ src/components/MovieCard/index.js
@@ -4,64 +4,61 @@ import { Card } from 'antd';
 import { StarFilled, DeleteOutlined } from '@ant-design/icons';
 import classes from '../MovieCard/styles.module.css';
 
-function MovieCard({ movie, addToFavoriteList, removeFromFavoriteList }) {
+function MovieCard(props) {
    let card = '';
-   if (removeFromFavoriteList) {
-      card = (
-         <Card
-            style={{ width: '100%', margin: '10px auto', background: 'rgba(51, 56, 61, 1)' }}
-            actions={[
-               <DeleteOutlined style={{ fontSize: '1.3rem' }}
 
-                  onClick={() => removeFromFavoriteList(movie.Title)}
-               />
-            ]}
-         >
-            <ul className={classes.dropDetails} >
-               <img alt="ups" src={movie.Poster} />
-               <li>
-                  <p>{movie.Title}</p>
-                  <p>year: {movie.Year}</p>
-                  <p>country: {movie.Country}</p>
-               </li>
-            </ul>
-         </Card>
-      );
-   } else {
-      card = (
-         <Card
-            hoverable
-            style={{ width: 340 }}
-            cover={
-               movie.Poster !== 'N/A' && <img alt='movie' src={movie.Poster} />
-            }
-         >
-            <ul className={classes.details}>
-               <h2>{movie.Title}
-                  <span>
-                     <StarFilled
-                        className='star'
-                        style={{ color: 'yellow', fontSize: '25px' }}
-                        onClick={() => addToFavoriteList(movie)}
-                     />
-                     <StarFilled
-                        style={{ color: 'yellow', fontSize: '25px' }}
-                        onClick={() => addToFavoriteList(movie)}
-                     />
-                     <StarFilled
-                        style={{ color: 'yellow', fontSize: '25px' }}
-                        onClick={() => addToFavoriteList(movie)}
-                     />
-                  </span>
-               </h2>
+   props.removeFromFavoriteList ? card = (
+      <Card
+         style={{ width: '100%', margin: '10px auto', background: 'rgba(51, 56, 61, 1)' }}
+         actions={[
+            <DeleteOutlined style={{ fontSize: '1.3rem' }}
 
-               <li>Year:  {movie.Year}</li>
-               <li>Rated: {movie.Rated}</li>
-               <li>Country:  {movie.Country}</li>
-            </ul>
-         </Card>
-      );
-   }
+               onClick={() => props.removeFromFavoriteList(props.movie.Title)}
+            />
+         ]}
+      >
+         <ul className={classes.dropDetails} >
+            <img alt="ups" src={props.movie.Poster} />
+            <li>
+               <p>{props.movie.Title}</p>
+               <p>year: {props.movie.Year}</p>
+               <p>country: {props.movie.Country}</p>
+            </li>
+         </ul>
+      </Card>
+   ) : card = (
+      <Card
+         hoverable
+         style={{ width: 340 }}
+         cover={
+            props.movie.Poster !== 'N/A' && <img alt='movie' src={props.movie.Poster} />
+         }
+      >
+         <ul className={classes.details}>
+            <h2>{props.movie.Title}
+               <span>
+                  <StarFilled
+                     className='star'
+                     style={{ color: 'yellow', fontSize: '25px' }}
+                     onClick={() => props.addToFavoriteList(props.movie)}
+                  />
+                  <StarFilled
+                     style={{ color: 'yellow', fontSize: '25px' }}
+                     onClick={() => props.addToFavoriteList(props.movie)}
+                  />
+                  <StarFilled
+                     style={{ color: 'yellow', fontSize: '25px' }}
+                     onClick={() => props.addToFavoriteList(props.movie)}
+                  />
+               </span>
+            </h2>
+
+            <li>Year:  {props.movie.Year}</li>
+            <li>Rated: {props.movie.Rated}</li>
+            <li>Country:  {props.movie.Country}</li>
+         </ul>
+      </Card>
+   )
    return <>{card}</>;
 }
 export default MovieCard;
