08a816ddac1b226615e6b2aea2ac99b5c9cec7c8
Mirko Zivic <mirkoduartes@gmail.com>
2022-12-16 10:19:50 +0100

    Deployed movie_database

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Routes from './routes';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const App = () => (
-   <Router>
+   <HashRouter>
       <Routes />
-   </Router>
+   </HashRouter>
 );
 
 export default App;
