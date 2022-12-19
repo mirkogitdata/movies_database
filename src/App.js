ee2e21abf737cded9ab0c3b86d56b25513a392dd
Mirko Zivic <mirkoduartes@gmail.com>
2022-12-19 20:37:03 +0100

    Maintain components structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,90 @@
-import React from 'react';
-import Routes from './routes';
-import { HashRouter } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import AuthContext from './context/auth-context';
+import { message } from 'antd';
+import api from './api/index';
+import SearchMovie from './pages/SearchMovie';
+import DetailsMovie from './pages/DetailsMovie';
+import FavoriteList from './pages/FavoriteList';
 
-const App = () => (
-   <HashRouter>
-      <Routes />
-   </HashRouter>
-);
+
+const App = () => {
+   const [loading, setLoading] = useState(false);
+   const [movie, setMovie] = useState(null);
+   const [favorite, setFavorite] = useState([]);
+   const [modalShow, setModalShow] = useState(false);
+
+   useEffect(() => {
+      const data = localStorage.getItem('my-favoriteList');
+      if (data) {
+         setFavorite(JSON.parse(data));
+      }
+   }, []);
+
+
+   useEffect(() => {
+      localStorage.setItem('my-favoriteList', JSON.stringify(favorite));
+   });
+
+   const fetchMovie = async (title) => {
+      await fetch(`${api.URL}/?t=${title}&apikey=${api.KEY}`)
+         .then((res) => res.json())
+         .then(async (response) => {
+            if (response.Response === 'False') {
+               message.warning('The movie is not found, please try again');
+            } else {
+               const movie = {
+                  imdbID: response.imdbID,
+                  Title: response.Title,
+                  Year: response.Year,
+                  Rated: response.Rated,
+                  Country: response.Country,
+                  Poster: response.Poster,
+                  Genre: response.Genre
+               };
+               setMovie(movie);
+            }
+         })
+         .catch((error) => console.log(error));
+      setLoading(false);
+   };
+
+
+   const addToFavoriteList = (movie) => {
+      if (!favorite.find((m) => m.Title === movie.Title)) {
+         setFavorite([...favorite, movie]);
+         message.success(`${movie.Title} is added to favorite movies`);
+      } else {
+         message.warn(`${movie.Title} has aleady been added`);
+      }
+   };
+   const removeFromFavoriteList = (title) => {
+      const newFavoriteList = favorite.filter((movie) => movie.Title !== title);
+      setFavorite(newFavoriteList);
+      message.success(`${title} is removed from favorite movies`);
+   };
+
+   return (
+      <AuthContext.Provider
+         value={{
+            movie: movie,
+            addToFavoriteList: addToFavoriteList,
+            modalShow: modalShow,
+            setModalShow: setModalShow,
+            favorite: favorite,
+            setFavorite: setFavorite,
+            removeFromFavoriteList: removeFromFavoriteList,
+            loading: loading,
+            setLoading: setLoading,
+            onFetchmovie: fetchMovie
+         }}
+      >
+         <SearchMovie />
+         <DetailsMovie />
+         <FavoriteList />
+      </AuthContext.Provider>
+   )
+}
+
+
 
 export default App;
--- src/pages/SearchMovie.js
+++ src/pages/SearchMovie.js
@@ -1,97 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { message, Divider } from 'antd';
-import AuthContext from '../context/auth-context';
-import api from '../api';
+import React from 'react';
+import { Divider } from 'antd';
 import SearchForm from '../components/SearchForm/SearchForm';
-import FavoriteList from './FavoriteList';
 import Container from '../components/Container/Container';
 import FavoriteBtn from '../components/Button/FavoriteBtn';
-import DetailsMovie from './DetailsMovie';
 
 const SearchMovie = () => {
-   const [loading, setLoading] = useState(false);
-   const [movie, setMovie] = useState(null);
-   const [favorite, setFavorite] = useState([]);
-   const [modalShow, setModalShow] = useState(false);
-
-   useEffect(() => {
-      const data = localStorage.getItem('my-favoriteList');
-      if (data) {
-         setFavorite(JSON.parse(data));
-      }
-   }, []);
-
-   useEffect(() => {
-      localStorage.setItem('my-favoriteList', JSON.stringify(favorite));
-   });
-
-   const fetchMovie = async (title) => {
-      await fetch(`${api.URL}/?t=${title}&apikey=${api.KEY}`)
-         .then((res) => res.json())
-         .then(async (response) => {
-            if (response.Response === 'False') {
-               message.warning('The movie is not found, please try again');
-            } else {
-               const movie = {
-                  imdbID: response.imdbID,
-                  Title: response.Title,
-                  Year: response.Year,
-                  Rated: response.Rated,
-                  Country: response.Country,
-                  Poster: response.Poster,
-                  Genre: response.Genre
-               };
-               setMovie(movie);
-            }
-         })
-         .catch((error) => console.log(error));
-      setLoading(false);
-   };
-
-
-   const addToFavoriteList = (movie) => {
-      if (!favorite.find((m) => m.Title === movie.Title)) {
-         setFavorite([...favorite, movie]);
-         message.success(`${movie.Title} is added to favorite movies`);
-      } else {
-         message.warn(`${movie.Title} has aleady been added`);
-      }
-   };
-   const removeFromFavoriteList = (title) => {
-      const newFavoriteList = favorite.filter((movie) => movie.Title !== title);
-      setFavorite(newFavoriteList);
-      message.success(`${title} is removed from favorite movies`);
-   };
 
 
    return (
       <>
-         <AuthContext.Provider value={{
-            movie: movie,
-            addToFavoriteList: addToFavoriteList,
-            modalShow: modalShow,
-            setModalShow: setModalShow,
-            favorite: favorite,
-            setFavorite: setFavorite,
-            removeFromFavoriteList: removeFromFavoriteList,
-            loading: loading,
-            setLoading: setLoading,
-            onFetchmovie: fetchMovie
-         }}>
-            <Container>
-               <Divider
-                  style={{ border: 'none', marginTop: '2rem' }}
-               />
-               <FavoriteBtn />
-               <FavoriteList />
-               <Divider
-                  style={{ border: 'none', marginTop: '9rem' }}
-               />
-               <SearchForm />
-            </Container>
-            <DetailsMovie />
-         </AuthContext.Provider>
+         <Container>
+            <Divider
+               style={{ border: 'none', marginTop: '2rem' }}
+            />
+            <FavoriteBtn />
 
+            <Divider
+               style={{ border: 'none', marginTop: '9rem' }}
+            />
+            <SearchForm />
+         </Container>
       </>
 
 
